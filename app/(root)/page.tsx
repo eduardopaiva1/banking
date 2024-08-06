a0e5dc24fde7e15f533be5203dd87d0706b93705
eduardopaiva1 <eduardo.racovski@gmail.com>
2024-08-05 23:47:18 -0300

    totalbalance and chart added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Eduardo'}
+
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                  type="greeting"
+                  title="Welcome"
+                  user={loggedIn?.firstName || 'Guest'}
+                  subtext="Acess and manage your account and transactions efficently"
+                />
+
+                <TotalBalanceBox 
+                  accounts={[]}
+                  totalBanks={1}
+                  totalCurrentBalance={1250.35}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
