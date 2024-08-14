5e978b92042b49542408fc89be05a88d20b02fb4
eduardopaiva1 <eduardo.racovski@gmail.com>
2024-08-13 22:43:19 -0300

    feat: db login and users actions

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,11 +22,13 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 
 const AuthForm = ({ type } : { type: string }) => {
   const [user, setUser] = useState(null)
   const [isLoading, setIsLoading ] = useState(false);
   const router = useRouter();
+  
 
   const formSchema = authFormSchema(type);
 
@@ -48,18 +50,18 @@ const AuthForm = ({ type } : { type: string }) => {
           // Sign up with Appwrite & create plaid token 
 
           if(type === 'sign-up') {
-            // const newUser = await signUp(data);
+            const newUser = await signUp(data);
 
-            // setUser(newUser)
+            setUser(newUser);
           }
 
           if(type === 'sign-in') {
-            // const response = await signIn({
-            //   email: data.email,
-            //   password: data.password
-            // })
+           const response = await signIn ({
+            email: data.email,
+            password: data.password,
+           })
 
-            // if(response) router.push('/')
+           if(response) router.push('/')
           }
         } catch (error) {
           console.log(error);
